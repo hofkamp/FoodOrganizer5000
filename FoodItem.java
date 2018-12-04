4de44044b787677c080e5c044ccfbd16581857d5
hofkamp <43786625+hofkamp@users.noreply.github.com>
2018-12-04 10:10:56 -0600

    Add files via upload

--- FoodItem.java
+++ FoodItem.java
@@ -1,46 +1,75 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class FoodItem<T> {
-	private String name;
-	private int cal;
-	private int carb;
-	private int pro;
-	private int fat;
-	private int fib;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getCal() {
-		return cal;
-	}
-	public void setCal(int cal) {
-		this.cal = cal;
-	}
-	public int getCarb() {
-		return carb;
-	}
-	public void setCarb(int carb) {
-		this.carb = carb;
-	}
-	public int getPro() {
-		return pro;
-	}
-	public void setPro(int pro) {
-		this.pro = pro;
-	}
-	public int getFat() {
-		return fat;
-	}
-	public void setFat(int fat) {
-		this.fat = fat;
-	}
-	public int getFib() {
-		return fib;
-	}
-	public void setFib(int fib) {
-		this.fib = fib;
-	}
-	
+/**
+ * This class represents a food item with all its properties.
+ * 
+ * @author aka
+ */
+public class FoodItem {
+    // The name of the food item.
+    private String name;
+
+    // The id of the food item.
+    private String id;
+
+    // Map of nutrients and value.
+    private HashMap<String, Double> nutrients;
+    
+    /**
+     * Constructor
+     * @param name name of the food item
+     * @param id unique id of the food item 
+     */
+    public FoodItem(String id, String name) {
+        // TODO : Complete
+    }
+    
+    /**
+     * Gets the name of the food item
+     * 
+     * @return name of the food item
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Gets the unique id of the food item
+     * 
+     * @return id of the food item
+     */
+    public String getID() {
+        // TODO : Complete
+        return null;
+    }
+    
+    /**
+     * Gets the nutrients of the food item
+     * 
+     * @return nutrients of the food item
+     */
+    public HashMap<String, Double> getNutrients() {
+        // TODO : Complete
+        return null;
+    }
+
+    /**
+     * Adds a nutrient and its value to this food. 
+     * If nutrient already exists, updates its value.
+     */
+    public void addNutrient(String name, double value) {
+        // TODO : Complete
+    }
+
+    /**
+     * Returns the value of the given nutrient for this food item. 
+     * If not present, then returns 0.
+     */
+    public double getNutrientValue(String name) {
+        // TODO : Complete
+        return 0;
+    }
+    
 }
