0f2737fa6c6283eed3c468b6f57d033184ed5d8f
scgerich <43677757+scgerich@users.noreply.github.com>
2018-12-04 15:55:58 -0600

    Update FoodItem.java

--- FoodItem.java
+++ FoodItem.java
@@ -1,3 +1,5 @@
+package finalProject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,16 +19,48 @@ public class FoodItem {
     // Map of nutrients and value.
     private HashMap<String, Double> nutrients;
     
+    //other fields
+    private int calories;
+	private int carbohydrates;
+	private int protein;
+	private int fat;
+	private int fiber;
+     
     /**
      * Constructor
-     * @param name name of the food item
-     * @param id unique id of the food item 
+     * initializes a meal item with all fields to 0 or empty
      */
-    public FoodItem(String id, String name) {
+	public FoodItem () {
+		this.name = "";
+        this.id = "";
+		this.calories = 0;
+		this.carbohydrates= 0;
+		this.fat = 0;
+		this.protein = 0;
+		this.fiber = 0;
+	    nutrients = new HashMap<String, Double>();
+	}
+     /**
+     * Constructor
+     * initializes the parameters to the values passed in
+     * @param String name
+     * @param int calories
+     * @param int carbs
+     * @param int fat
+     * @param int protein
+     * @param int fiber
+     * @param ArrayList ingr
+     */
+	public FoodItem (String name, String id, int calories, int carbs, int fat, int protein, int fiber) {
+		this.id = id;
         this.name = name;
-        this.id = id;
+		this.calories = calories;
+		this.carbohydrates= carbs;
+		this.fat = fat;
+		this.protein = protein;
+		this.fiber = fiber;
         nutrients = new HashMap<String, Double>();
-    }
+	} 
     
     /**
      * Gets the name of the food item
@@ -36,6 +70,88 @@ public class FoodItem {
     public String getName() {
         return name;
     }
+    
+    /**
+     * Sets the name of the food
+     */
+	public void setName(String name) {
+		this.name = name;
+	}
+    	
+	/**
+     * Gets the amount of calories in the foodItem
+     * @return int calories
+     */
+	public int getCal() {
+		return calories;
+	}
+	
+	/**
+     * Sets the number of calories in the foodItem
+     */
+	public void setCal(int cal) {
+		this.calories = cal;
+	}
+	
+	/**
+     * Gets the amount of carbohydrates in the foodItem
+     * @return int carbohydrates
+     */
+	public int getCarb() {
+		return carbohydrates;
+	}
+	
+	/**
+     * Sets the amount of carbohydrates in the foodItem
+     */
+	public void setCarb(int carb) {
+		this.carbohydrates = carb;
+	}
+	
+	/**
+     * Gets the amount of fat in the foodItem
+     * @return int fat
+     */
+	public int getFat() {
+		return fat;
+	}
+	
+	/**
+     * Sets the amount of fat in the foodItem
+     */
+	public void setFat(int fat) {
+		this.fat = fat;
+	}
+	
+	/**
+     * Gets the amount of protein in the foodItem
+     * @return int protein
+     */
+	public int getPro() {
+		return protein;
+	}
+	
+	/**
+     * Sets the amount of protein in the foodItem
+     */
+	public void setPro(int pro) {
+		this.protein = pro;
+	}
+	
+	/**
+     * Gets the amount of fiber in the foodItem
+     * @return int fiber
+     */
+	public int getFib() {
+		return fiber;
+	}
+	
+	/**
+     * Sets the amount of fiber in the foodItem
+     */
+	public void setFib(int fib) {
+		this.fiber = fib;
+	}
 
     /**
      * Gets the unique id of the food item
