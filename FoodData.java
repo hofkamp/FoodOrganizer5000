6d069db44e508a3d9c097750d0925353d3246cd9
scgerich <43677757+scgerich@users.noreply.github.com>
2018-12-04 22:32:12 -0600

    Update FoodData.java

--- FoodData.java
+++ FoodData.java
@@ -16,11 +16,12 @@ public class FoodData implements FoodDataADT<FoodItem> {
     private HashMap<String, BPTree<Double, FoodItem>> indexes;
     
     
-    /**
+     /**
      * Public constructor
      */
     public FoodData() {
-        // TODO : Complete
+        foodItemList = new ArrayList<>();
+        indexes = new HashMap<String, BPTree<Double,FoodItem>>();
     }
     
     
@@ -30,7 +31,35 @@ public class FoodData implements FoodDataADT<FoodItem> {
      */
     @Override
     public void loadFoodItems(String filePath) {
-        // TODO : Complete
+        Path pathToFile = Paths.get(filePath);
+        
+        //create a buffered reader using try with resources
+        try(BufferedReader reader = Files.newBufferedReader(pathToFile)){
+        		
+        	//read the first line of the file
+        		String line = reader.readLine();
+        		
+        	//read all lines of the file
+        		while(line != null) {
+        			//split the string in to an array of values using the comma as a delimiter
+        			
+        			String[] foodArray = line.split(",");
+        			FoodItem food = new FoodItem(foodArray[0], foodArray[1]);
+        			
+        			//add the nutrients to the food object
+        			food.addNutrient(foodArray[2], Double.parseDouble(foodArray[3]));
+        			food.addNutrient(foodArray[4], Double.parseDouble(foodArray[5]));
+        			food.addNutrient(foodArray[6], Double.parseDouble(foodArray[7]));
+        			food.addNutrient(foodArray[8], Double.parseDouble(foodArray[9]));
+        			food.addNutrient(foodArray[10], Double.parseDouble(foodArray[11]));
+        			
+        			foodItemList.add(food);
+        			line = reader.readLine();
+        		}
+        	
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
     /*
@@ -39,8 +68,19 @@ public class FoodData implements FoodDataADT<FoodItem> {
      */
     @Override
     public List<FoodItem> filterByName(String substring) {
-        // TODO : Complete
-        return null;
+    		//create a list object to store the filtered items
+        List<FoodItem> filteredItems = new ArrayList<>();
+        
+        //for loop to parse through all the food objects 
+        for(int i = 0; i<foodItemList.size(); i++ ) {
+        		FoodItem food = foodItemList.get(i);
+        		//if the food's name contains the substring, it is added to the filtered list
+        		if(food.getName().toLowerCase().contains(substring.toLowerCase())) {
+        			filteredItems.add(food);
+        		}
+        }
+        return filteredItems;
+       
     }
 
     /*
