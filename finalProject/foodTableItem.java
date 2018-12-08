5f4b2858bef64c49300516101faafaa6af10a999
hofkamp <43786625+hofkamp@users.noreply.github.com>
2018-12-08 14:44:09 -0600

    Add files via upload
    needed for displaying food in table

---
+++ finalProject/foodTableItem.java
@@ -0,0 +1,82 @@
+package finalProject;
+
+import finalProject.FoodItem;
+
+//used for displaying food items in table cuz you can't display
+//a BPTree
+public class foodTableItem {
+	private String name;
+	private String id;
+	private String calories;
+	private String carbohydrates;
+	private String fat;
+	private String fiber;
+	private String protein; 
+	
+	public foodTableItem(FoodItem food) {
+		this.name = food.getName();
+		this.id = food.getID();
+		this.calories = food.getNutrientValue("calories") + "";
+		this.carbohydrates = food.getNutrientValue("carbohydrate") + "";
+		this.fat = food.getNutrientValue("fat") + "";
+		this.fiber = food.getNutrientValue("fiber") + "";
+		this.protein = food.getNutrientValue("protein") + "";
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCalories() {
+		return calories;
+	}
+
+	public void setCalories(String calories) {
+		this.calories = calories;
+	}
+
+	public String getCarbohydrates() {
+		return carbohydrates;
+	}
+
+	public void setCarbohydrates(String carbohydrates) {
+		this.carbohydrates = carbohydrates;
+	}
+
+	public String getFat() {
+		return fat;
+	}
+
+	public void setFat(String fat) {
+		this.fat = fat;
+	}
+
+	public String getFiber() {
+		return fiber;
+	}
+
+	public void setFiber(String fiber) {
+		this.fiber = fiber;
+	}
+
+	public String getProtein() {
+		return protein;
+	}
+
+	public void setProtein(String protein) {
+		this.protein = protein;
+	}
+}
