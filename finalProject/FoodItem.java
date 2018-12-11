7d665bbeadb46da6069ee21b632ba7db67a3a52f
scgerich <43677757+scgerich@users.noreply.github.com>
2018-12-11 01:14:09 -0600

    Commented

--- finalProject/FoodItem.java
+++ finalProject/FoodItem.java
@@ -1,6 +1,17 @@
+/**
+ * Filename:   FoodItem.java
+ * Project:    Final Project
+ * Authors:    Thomas Antonacci, Sally Gerich, Kelsey Hickok, William Hofkamp, Apostolos Velliotis
+ *
+ * Semester:   Fall 2018
+ * Course:     CS400
+ * 
+ * Due Date:   12/12/18
+ * Version:    Milestone 3
+ * 
+ * Bugs:       No known bugs exist
+ */
 package finalProject;
-
-
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,7 +20,7 @@ import java.util.List;
 /**
  * This class represents a food item with all its properties.
  * 
- * @author aka
+ * @authors Thomas Antonacci, Sally Gerich, Kelsey Hickok, William Hofkamp, Apostolos Velliotis
  */
 public class FoodItem {
     // The name of the food item.
@@ -23,7 +34,7 @@ public class FoodItem {
     
      
     /**
-     * Constructor
+     * Constructor for the FoodItem class
      * @param name name of the food item
      * @param id unique id of the food item 
      */
@@ -62,21 +73,28 @@ public class FoodItem {
     }
 
     /**
-     * Adds a nutrient and its value to this food. 
+     * Adds a nutrient and its value to the food item 
      * If nutrient already exists, updates its value.
+     * @param String name of the nutrient
+     * @param double value of the nutrient
      */
     public void addNutrient(String name, double value) {
         nutrients.put(name, value);
     }
 
     /**
-     * Returns the value of the given nutrient for this food item. 
-     * If not present, then returns 0.
+     * Returns the value of the given nutrient for the food item. 
+     * If not present, it returns 0.
+     * @param String name of nutrient
+     * @return the double value of the nutrient for the food item
      */
     public double getNutrientValue(String name) {
         return nutrients.get(name);
     }
     
+    /**
+     * toString method
+     */
     @Override
     public String toString() {
         return this.name;
