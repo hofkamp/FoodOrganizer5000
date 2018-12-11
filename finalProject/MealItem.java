d31e02d1cbd8d41e093b6a90f08343f64b78c0bd
scgerich <43677757+scgerich@users.noreply.github.com>
2018-12-11 01:22:54 -0600

    Commented

--- finalProject/MealItem.java
+++ finalProject/MealItem.java
@@ -1,9 +1,25 @@
+/**
+ * Filename:   MealItem.java
+ * Project:    Final Project
+ * Authors:    Thomas Antonacci, Sally Gerich, Kelsey Hickok, William Hofkamp, Apostolos Velliotis
+ *
+ * Semester:   Fall 2018
+ * Course:     CS400
+ * 
+ * Due Date:   12/12/18
+ * Version:    Milestone 3
+ * 
+ * Bugs:       No known bugs exist
+ */
+
 package finalProject;
-
-
-
 import java.util.ArrayList;
 
+/**
+ * This class represents a meal item with all its properties.
+ * 
+ * @authors Thomas Antonacci, Sally Gerich, Kelsey Hickok, William Hofkamp, Apostolos Velliotis
+ */
 public class MealItem <T> {
 	//fields
 	private String name;
@@ -15,7 +31,7 @@ public class MealItem <T> {
 	private ArrayList<FoodItem> ingredients = new ArrayList();
 	
 	
-	  /**
+	 /**
      * Constructor
      * initializes a meal item with all fields to 0 or empty
      */
@@ -31,6 +47,9 @@ public class MealItem <T> {
 		
 	}
 	
+	/**
+     * Calculates the total nutrient value for a meal
+     */
 	 public void calcNutrients() {
 		 for(int i = 0; i < ingredients.size(); i++) {
 			 calories += ingredients.get(i).getNutrientValue("calories");
@@ -52,6 +71,7 @@ public class MealItem <T> {
 
 	/**
      * Sets the value of the arrayList for the ingredients
+     * @param ArrayList ingr
      */
 	public void setIngr(ArrayList ingr) {
 		this.ingredients = ingredients;
@@ -67,6 +87,7 @@ public class MealItem <T> {
 	
 	/**
      * Sets the name of the meal
+     * @param String name 
      */
 	public void setName(String name) {
 		this.name = name;
@@ -82,6 +103,7 @@ public class MealItem <T> {
 	
 	/**
      * Sets the number of calories in the meal
+     * @param int cal
      */
 	public void setCal(int cal) {
 		this.calories = cal;
@@ -97,6 +119,7 @@ public class MealItem <T> {
 	
 	/**
      * Sets the amount of carbohydrates in the meal
+     * @param carb
      */
 	public void setCarb(int carb) {
 		this.carbohydrate = carb;
@@ -112,6 +135,7 @@ public class MealItem <T> {
 	
 	/**
      * Sets the amount of fat in the meal
+     * @param int fat
      */
 	public void setFat(int fat) {
 		this.fat = fat;
@@ -127,6 +151,7 @@ public class MealItem <T> {
 	
 	/**
      * Sets the amount of protein in the meal
+     * @param int pro
      */
 	public void setPro(int pro) {
 		this.protein = pro;
@@ -142,10 +167,10 @@ public class MealItem <T> {
 	
 	/**
      * Sets the amount of fiber in the meal
+     * @param int fib
      */
 	public void setFib(int fib) {
 		this.fiber = fib;
 	}
-	
 	
 }
